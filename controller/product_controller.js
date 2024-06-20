95528a3119d8f1af2a34d9e4d2141890ba4c0e63
Dinesh Singh <dkadesh1999@gmail.com>
2024-06-20 23:34:43 +0530

    this

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -4,10 +4,21 @@ const Subcategory = require("../models/productSubCategory");
 
 exports.createProduct = async (req, resp) => {
   try {
-    const { name, title, price, ratings, color, size, offer, photos, SubcategoryId } =
+    console.log('Headers:', req.headers);
+    console.log('Request body:', req.body);
+    console.log('File:', req.file);
+
+    if (!req.file) {
+      return resp.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const { name, title, price, ratings, color, size, offer, SubcategoryId } =
       req.body;
 
-    const thumbnail = req.files.thumbnail;
+      
+    const photos = req.file.path;
+
+    // const thumbnail = req.files.thumbnail;
     
 
     // const userId = req.user.id;
@@ -21,27 +32,30 @@ exports.createProduct = async (req, resp) => {
 
     //   see the category is valid or not
 
-    const SubCategoryDetails = await Product.findOne({
-      _id: SubcategoryId,
-    });
+    // const SubCategoryDetails = await Product.findOne({
+    //   _id: SubcategoryId,
+    // });
 
-    if (!SubCategoryDetails) {
-      return resp.status(404).json({
-        success: false,
-        messages: "sub category details are not found",
-      });
-    }
+    // if (!SubCategoryDetails) {
+    //   return resp.status(404).json({
+    //     success: false,
+    //     messages: "sub category details are not found",
+    //   });
+    // }
 
     // upload to cloudinary
 
-    const images = await uploadTocloudinary(
-      thumbnail,
-      process.env.CLOUDINARY_URL,
-      1000,
-      1000
-    );
 
-    const product = await Product.create({
+
+    // const images = await uploadTocloudinary(
+    //   thumbnail,
+    //   process.env.CLOUDINARY_URL,
+    //   1000,
+    //   1000
+    // );
+
+
+    const productItem = await Product({
       name,
       title,
       price,
@@ -50,28 +64,29 @@ exports.createProduct = async (req, resp) => {
       size,
       offer,
       photos,
-      thumbnail: images.secure_url,
+      // thumbnail: images.secure_url,
       // postedBy: userId,
-      subcategory: SubCategoryDetails._id,
+      // subcategory: SubCategoryDetails._id,
     });
 
     // add course entry in Category => because us Category ke inside sare course aa jaye
 
-    await Subcategory.findByIdAndUpdate(
-      { _id: SubCategoryDetails._id },
-      {
-        $push: {
-          products: product._id,
-        },
-      },
-      { new: true }
-    );
-
+    // await Subcategory.findByIdAndUpdate(
+    //   { _id: SubCategoryDetails._id },
+    //   {
+    //     $push: {
+    //       products: product._id,
+    //     },
+    //   },
+    //   { new: true }
+    // );
+    await productItem.save();
     return resp.status(200).json({
       success: true,
       messages: "successfully created the product",
-      product,
+      productItem,
     });
+    console.log(productItem);
   } catch (error) {
     console.log(error);
     resp.status(500).json({
